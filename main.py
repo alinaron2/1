46122e30d0be3a93461364072bce78a0ec63719e
alinaron2 <your.email@example.com>
2024-09-20 16:07:38 +0200

    Test1

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Create a list of numbers from 1 to 10
+numbers = list(range(1, 11))
+
+# Create a numpy array from the list
+array = np.array(numbers)
+
+# Print the numpy array
+print(array)
